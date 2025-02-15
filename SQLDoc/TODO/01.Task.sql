f1786a10095da792757f769060ce8f92e27cddf0
Your Name <alejoab1020@gmail.com>
2025-02-15 11:27:49 -0500

    Tarea propuesta

---
+++ SQLDoc/TODO/01.Task.sql
@@ -0,0 +1,10 @@
+/*
+1. Separar de la tabla Carros el color
+2. Relacionar la tabla CArros con el color
+3. Crar FK para la tabla Carro relacionado con IdColor
+4. Relacionar el modelo con la marca
+5 Relacionar el modelo con la tabla Carro
+6. Hacer el Inner JOIN
+7. Consultar CROSS JOIN, LEFT JOIN, INER JOIN, RIGHT JOIN
+
+*/
